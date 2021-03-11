3b8f3648b05c4746657f67b8f0f099ccae1ad4bf
thuando <thuan.dh1412@gmail.com>
2021-03-11 20:58:43 +0700

    fix: fix gitlab-ci.yml

--- .gitlab-deploy-prod.sh
+++ .gitlab-deploy-prod.sh
@@ -2,13 +2,19 @@
 
 # set server list
 set -f
-string=$PROD_DEPLOY_SERVER
-array=(${string//,/ })
+# string=$PROD_DEPLOY_SERVER
+# array=(${string//,/ })
 
-echo 
+# echo 
+ls ~/.ssh
+echo "start ssh"
+# echo $PROD_DEPLOY_SERVER
+cat ~/.ssh/id_rsa
+ssh -i ~/.ssh/id_rsa ubuntu@18.191.78.155 "cd dubbing-backend && git pull --force origin feat/set-up-ci-cd && sudo docker-compose restart"
+echo "DONE!!!!!"
 
 # iterate..
-for i in "${!array[@]}"; do
-    echo "Deploy on... ${array[i]}"
-    ssh ubuntu@${array[i]} "cd dubbing-backend && git pull origin feat/set-up-ci-cd && sudo docker-compose restart"
-done
+# for i in "${!array[@]}"; do
+#     echo "Deploy on... ${array[i]}"
+#     ssh ubuntu@${array[i]} "cd dubbing-backend && git pull origin feat/set-up-ci-cd && sudo docker-compose restart"
+# done
